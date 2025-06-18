16eb3ced950143f43b58d7d14e9faae8147c08ac
Ron Bokleman <ron.bokleman@bluemetal.com>
2025-06-18 11:52:36 -0400

    New Security Group for VPC using module

--- main.tf
+++ main.tf
@@ -22,13 +22,30 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  # Add aws security group to the VPC the EC2 instance lives in as defined below.
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  # # Add aws security group to the VPC the EC2 instance lives in as defined below.
+  # vpc_security_group_ids = [aws_security_group.blog.id]
 
+  # Add aws security group to the VPC the EC2 instance lives in as defined using the module.
+  vpc_security_group_ids = [module.blog-sg.security_group_id]
+  
   tags = {
     Name = "Learning Terraform"
   }
 }
+
+module "blog-sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name = "blog_new"
+
+  vpc_id              = data.aws_vpc.default.id
+  ingress_rules       = ["http-80-tcp,https-443-tcp"]
+  ingress_cidr_blocks = [ "0.0.0.0/0" ]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = [ "0.0.0.0/0" ]
+}
+
 
 # Create new aws_security_group in the default vpc.
 resource "aws_security_group" "blog" {
